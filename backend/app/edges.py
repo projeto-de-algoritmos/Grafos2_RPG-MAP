6419caf7fc40adaefd612d95f1d61765252b139d
mateusmaiamaia <mateusmaiamaia@hotmail.com>
2022-07-11 18:39:56 -0300

    feat: create graph base
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

---
+++ backend/app/edges.py
@@ -0,0 +1,90 @@
+countries = {
+    "Mymeria":[
+        ("Velugauth",1),
+    ],
+    "Velugauth":[
+        ("Mymeria",1),
+        ("Ayvyan",3)
+    ],
+    "Ayvyan":[
+        ("Velugauth",3),
+        ("Chach",3),
+        ("Haksun",4)
+    ],
+    "Haksun": [
+        ("Chach",4),
+        ("Ayvyan",4),
+        ("Abukastan",2)
+    ],
+    "Chach": [
+        ("Ecal",5),
+        ("Emerian",6),
+        ("Ayvyan",3),
+        ("Haksun",4)
+    ],
+    "Ecal": [
+        ("Shavew",2),
+        ("Chach",5)
+    ],
+    "Shavew": [
+        ("Emerian",5),
+        ("Ecal",2),
+        ("Raulheya",10)
+    ],
+    "Emerian":[
+        ("Moshearia",5),
+        ("Chach",6),
+        ("Shavew",5)
+    ],
+    "Abukastan": [
+        ("Haksun",2),
+    ],
+    "Moshearia": [
+        ("Stormitia",3),
+        ("Emerian",5)
+    ],
+    "Stormitia": [
+        ("Curwich",1),
+        ("Storemon",3)
+    ],
+    "Curwich": [
+        ("Stormitia",1)
+    ],
+    "Storemon": [
+        ("Stormitia",3),
+        ("Lithmyridhmy",4),
+        ("Ulheilaurt",5)
+    ],
+    "Lithmyridhmy": [
+        ("Storemon",4),
+        ("Ulheilaurt",3)
+    ],
+    "Ulheilaurt": [
+        ("Storemon",5),
+        ("Lithmyridhmy",3),
+        ("Chaghed",3),
+        ("Smot",5)
+    ],
+    "Smot": [
+        ("Raulheya",2),
+        ("Ymdonyuer",2),
+        ("Methe",3),
+        ("Ulheilaurt",5)
+    ],
+    "Raulheya": [
+        ("Smot",2),
+        ("Shavew",10),
+        ("Ymdonyuer",2)
+    ],
+    "Ymdonyuer": [
+        ("Raulheya",2),
+        ("Smot",2)
+    ],
+    "Methe": [
+        ("Smot",3)
+    ],
+    "Chaghed": [
+        ("Ulheilaurt",3)
+    ]
+
+}
